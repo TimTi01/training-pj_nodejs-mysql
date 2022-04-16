53f1725e86df7bfa31cb0c6721722a8f20e5b6ec
Artem Fadeev <TimF1234@yandex.ru>
2022-04-16 21:35:13 +0300

    first commit

---
+++ Controller/UsersController.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const response = require('./../response')
+
+exports.users = (req, res) => {
+
+    const users = [
+        {
+            "id": 1,
+            "name": "Alex",
+        },
+        {
+            "id": 2,
+            "name": "Otto",
+        },
+    ]
+
+    response.status(users, res)
+}
